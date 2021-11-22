35f9565d682a90efa6d488e22394f07b1302dc6f
djibadiawara <djiba.diawara97@gmail.com>
2021-11-22 12:06:23 -0500

    commit tree 3a6011be695cfc58c20939f3064a87de00cc42cd

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+let random = Math.ceil(Math.random() * 10)
+let num3 = 12
+let num4 = 3
+let mod = num3 / num4
+let max =(5, 10, 20) 
 
-// Write your code here
